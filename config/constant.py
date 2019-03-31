08e671f38bd3373de3a2b889a2074c2e209d64c3
allen <kaodondon7@gmail.com>
2019-03-31 23:41:04 +0800

    reference constant variable instead of heard code  as do as possible
    add constant.py

---
+++ config/constant.py
@@ -0,0 +1,59 @@
+# put strategy
+linear_response = 'linear_response'
+fibonacci_base = 'fibonacci_base'
+foo_double = 'foo_double'
+kelly = 'kelly'
+
+# bet strategy
+random = 'random'
+as_last = 'as_last'
+keep_false = 'keep_false'
+keep_true = 'keep_true'
+
+# columns of battle_statistic dataframe
+current_put = 'current_put'
+win_result = 'win_result'
+current_response = 'current_response'
+subtotal = 'subtotal'
+actual_win = 'actual_win'
+expected_win = 'expected_win'
+
+# columns of strategy dataframe
+expected_win_unit = 'expected_win_unit'
+current_put_unit = 'current_put_unit'
+accumulative_put_unit = 'accumulative_put_unit'
+win_response_unit = 'win_response_unit'
+subtotal_unit = 'subtotal_unit'
+accumulative_put = 'accumulative_put'
+win_response = 'win_response'
+
+# config
+support_strategy = 'support_strategy'
+gambling = 'gambling'
+combination = 'combination'
+ratio_per_game = 'ratio_per_game'
+bet_base = 'bet_base'
+DB = 'DB'
+user = 'user'
+password = 'password'
+host = 'host'
+schema = 'schema'
+
+# columns of table player_summarize
+player_summarize = 'player_summarize'
+player_id = 'player_id'
+strategy = 'strategy'
+initial_money = 'initial money'
+still_survival = 'still_survival'
+win_ratio = 'win_ratio'
+max_continuous_lose_count = 'max_continuous_lose_count'
+final_money = 'final_money'
+final_result = 'final_result'
+
+# columns of table gambling summarize
+gambling_summarize = 'gambling_summarize'
+win_player_ratio = 'win_player_ratio'
+average_player_win = 'average_player_win'
+survival_player_ratio = 'survival_player_ratio'
+final_banker_money = 'final_banker_money'
+all_strategy = 'all'
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from simulator import Simulator
+from config import constant
 
 
 def main():
-    game_times = 1000
-    strategy_list = ['kelly', 'linear_response', 'fibonacci_base', 'foo_double'] * 20
+    game_times = 100
+    strategy_list = [constant.kelly, constant.linear_response, constant.fibonacci_base, constant.foo_double] * 10
     simulator = Simulator(play_times=game_times, number_of_player=len(strategy_list), player_init_money=300000,
                           to_db=True, player_strategy=strategy_list)
     simulator.start_simulation()
