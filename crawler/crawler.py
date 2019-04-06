7c98631453546701f6dca182a9481f44526a7a6e
allen <kaodondon7@gmail.com>
2019-04-07 03:00:30 +0800

    crawler support write to db

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,14 +1,21 @@
+import datetime
+import re
+from collections import defaultdict
+
+import pandas as pd
+import pymysql
 import requests
 import yaml
-import re
-from crawler import constant
 from bs4 import BeautifulSoup
+from sqlalchemy import create_engine
+
+from config import string_constant
 from config.logger import get_logger
-from collections import defaultdict
+from crawler import constant
 
 
 class Crawler(object):
-    def __init__(self, start_date, total_date=None, end_date=None):
+    def __init__(self, start_date, total_day=None, end_date=None):
         self.logger = get_logger(self.__class__.__name__)
         with open('config/configuration.yml') as config:
             self.config = yaml.load(config)
@@ -19,6 +26,23 @@ class Crawler(object):
         self.prediction_info_all_prefer = defaultdict(list)
         self.prediction_info_top_100 = defaultdict(list)
 
+        self.start_date = datetime.datetime.strptime(start_date, '%Y%m%d')
+        self.total_date = total_day
+        self.end_date = self.start_date + datetime.timedelta(
+            total_day) if total_day is not None else datetime.datetime.strptime(end_date, '%Y%m%d')
+
+        # init db
+        user = self.config[string_constant.DB][string_constant.user]
+        password = self.config[string_constant.DB][string_constant.password]
+        host = self.config[string_constant.DB][string_constant.host]
+        self.engine = create_engine('mysql+pymysql://{}:{}@{}'.format(user, password, host))
+        self.db = pymysql.connect(host=host, user=user, passwd=password,
+                                  db=self.config[string_constant.DB][string_constant.schema], charset='utf8')
+
+    def start_crawler(self):
+        for date in pd.date_range(start=self.start_date, end=self.end_date):
+            self.get_game_data(datetime.datetime.strftime(date, '%Y%m%d'))
+
     def get_game_data(self, date):
         for member_type in self.config['crawler']['memberType'].values():
             res = requests.get(self.get_url(date, member_type))
@@ -27,20 +51,22 @@ class Crawler(object):
             custom_row = True
             for row_content in soup.find('tbody').findAll('tr', {'class': 'game-set'}):
                 if custom_row:
-                    self.append_game_id(row_content)
+                    assert self.check_data_consistent()
+                    self.append_game_id(row_content, date)
                     self.append_game_time(row_content)
                     self.append_team_name(row_content)
                     self.append_score(row_content)
+                    self.append_total_point_info(row_content)
                 self.append_point_spread_info(row_content, custom_row)
-
+                self.append_response_ratio_info(row_content, custom_row)
                 custom_row = not custom_row
-                assert self.check_data_consistent()
+            self.write_to_db(pd.DataFrame.from_dict(self.game_info))
             return
 
-    def append_game_id(self, row_content):
+    def append_game_id(self, row_content, date):
         self.logger.info(
             'current column size of {}: {}'.format(constant.game_id, len(self.game_info[constant.game_id])))
-        game_id = row_content.find('td', 'td-gameinfo').find('h3').text
+        game_id = date + row_content.find('td', 'td-gameinfo').find('h3').text
         self.logger.info('append game id: {}'.format(game_id))
         self.game_info[constant.game_id].append(game_id)
         return
@@ -67,16 +93,16 @@ class Crawler(object):
 
     def append_team_name(self, row_content):
         self.logger.info(
-            'current column size of {}: {}'.format(constant.guest_id, len(self.game_info[constant.guest_id])))
+            'current column size of {}: {}'.format(constant.guest_id, len(self.game_info[constant.guest])))
         self.logger.info(
-            'current column size of {}: {}'.format(constant.host_id, len(self.game_info[constant.host_id])))
+            'current column size of {}: {}'.format(constant.host_id, len(self.game_info[constant.host])))
 
         guest = row_content.find('td', {'class': 'td-teaminfo'}).find_all('tr')[0].find('a').text.strip()
         host = row_content.find('td', {'class': 'td-teaminfo'}).find_all('tr')[-1].find('a').text.strip()
 
         self.logger.debug('append guest: {}, host: {}'.format(guest, host))
-        self.game_info[constant.guest_id].append(guest)
-        self.game_info[constant.host_id].append(host)
+        self.game_info[constant.guest].append(constant.team_name_mapping[guest])
+        self.game_info[constant.host].append(constant.team_name_mapping[host])
         return
 
     def get_prediction_data(self, row_text_list):
@@ -98,49 +124,59 @@ class Crawler(object):
             self.game_info[constant.response_if_meet_spread_point].append(hit_percentage)
 
         if custom_row:
-            # get national total point info
-            national_total_point = row_content.find('td', {'class': 'td-universal-bet02'}).text.strip()
-            national_total_point = re.findall(r'\d+\.\d+', national_total_point)[0]  # filter our first float
-            self.game_info[constant.national_total_point].append(float(national_total_point))
-
-            # get local point spread info
+            # get local point spread info and response ratio
             local_host_spread_point = row_content.find('td', {'class': 'td-bank-bet01'}).text.strip()
             # filter out float
-            local_host_spread_point, spread_point_response_ratio = re.findall(r'[+-]?\d+\.\d+', local_host_spread_point)
+            data = re.findall(r'[+-]?\d+\.\d+', local_host_spread_point)
+            local_host_spread_point, spread_point_response_ratio = data if len(data) == 2 else 0
             self.game_info[constant.local_host_point_spread].append(float(local_host_spread_point))
             self.game_info[constant.local_host_point_spread_response_ratio].append(float(spread_point_response_ratio))
 
-            # get local total point info
+            # get local total point info and response ratio
             local_total_point = row_content.find('td', {'class': 'td-bank-bet02'}).text.strip()
             # filter out float
-            local_total_point, total_point_response = re.findall(r'\d+\.\d+', local_total_point)
+            data = re.findall(r'\d+\.\d+', local_total_point)
+            local_total_point, total_point_response = data if len(data) == 2 else 0
             self.game_info[constant.local_total_point_threshold].append(float(local_total_point))
             self.game_info[constant.local_total_point_threshold_response_ratio].append(float(total_point_response))
 
+        return
+
+    def append_total_point_info(self, row_content):
+        # get national total point info
+        national_total_point = row_content.find('td', {'class': 'td-universal-bet02'}).text.strip()
+        threshold = re.findall(r'\d+\.\d+', national_total_point)
+        national_total_point = threshold[0] if threshold else 0
+        self.game_info[constant.national_total_point].append(float(national_total_point))
+        pass
+
+    def append_response_ratio_info(self, row_content, guest_row):
+        self.logger.info('append response ratio info')
+        if guest_row:
             # get guest response ratio of no point spread at local
             local_origin_guest_response_ratio = row_content.find('td', {'class': 'td-bank-bet03'}).text.strip()
-            local_origin_guest_response_ratio = re.findall(r'\d+\.\d+', local_origin_guest_response_ratio)[0]
+            ratio = re.findall(r'\d+\.\d+', local_origin_guest_response_ratio)
+            local_origin_guest_response_ratio = ratio[0] if ratio else 0
             self.game_info[constant.local_origin_guest_response_ratio].append(float(local_origin_guest_response_ratio))
         else:
             # get host response ratio of no point spread at local
             local_origin_host_response_ratio = row_content.find('td', {'class': 'td-bank-bet03'}).text.strip()
-            local_origin_host_response_ratio = re.findall(r'\d+\.\d+', local_origin_host_response_ratio)[0]
+            ratio = re.findall(r'\d+\.\d+', local_origin_host_response_ratio)
+            local_origin_host_response_ratio = ratio[0] if ratio else 0
             self.game_info[constant.local_origin_host_response_ratio].append(float(local_origin_host_response_ratio))
 
+    def write_to_db(self, game_data):
+        self.logger.info('start write game data to db')
+        game_data.to_sql(con=self.engine,
+                         name='game_info',
+                         if_exists='append',
+                         schema=self.config[string_constant.DB][string_constant.schema],
+                         index_label='game_id')
         return
 
     def check_data_consistent(self):
         length_list = [len(i) for i in self.game_info.values()]
-        return length_list.count(length_list[0]) == len(length_list)
+        return length_list.count(length_list[0]) == len(length_list) if length_list else True
 
     def get_url(self, date, member_type=1):
         return self.config['crawler']['urlPattern'].format(date, member_type)
-
-
-if __name__ == '__main__':
-    # url = 'https://www.playsport.cc/predictgame.php?action=scale&allianceid=3&gametime=20190402&sid=0'
-    # get_game_result(url)
-    a = Crawler('20190401')
-    a.get_game_data('20190401')
-    b = a.get_url('20190401', 0)
-    print('fwefw')
