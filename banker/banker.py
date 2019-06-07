2472eb6ec06ec040b186a866e922996fe99f7384
Allen <kaodondon7@gmail.com>
2019-06-07 18:45:34 +0800

    add Banker readme

--- banker/banker.py
+++ banker/banker.py
@@ -4,3 +4,4 @@ import numpy as np
 class Banker(object):
     def __init__(self, play_times, combination=1):
         self.game_result = np.random.randint(2, size=play_times * combination).reshape(play_times, combination)
+        # TODO: add option to get game_result from DB
