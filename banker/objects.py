76dc0a97a29ab729884d8b18efe54571e7ab6dc4
allen <kaodondon7@gmail.com>
2020-02-24 02:17:57 +0800

    code refine

--- banker/objects.py
+++ banker/objects.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 class GambleInfo:
     def __init__(self, gamble_info):
-        self.gamble_id = gamble_info['game_id']
+        self.gamble_id = gamble_info['gamble_id']
         self.game_time = gamble_info['game_time']
         self.game_date = str(datetime.strptime(self.game_time[:10], '%Y-%m-%d').date().strftime('%Y%m%d'))
         self.game_type = gamble_info['game_type']
@@ -15,7 +15,7 @@ class GambleInfo:
 
 class GambleResult:
     def __init__(self, gamble_info):
-        self.gamble_id = gamble_info['game_id']
+        self.gamble_id = gamble_info['gamble_id']
         self.game_time = gamble_info['game_time']
         self.game_type = gamble_info['game_type']
         self.guest = gamble_info['guest']
--- gambler/gambler.py
+++ gambler/gambler.py
@@ -4,61 +4,60 @@ from banker.banker import Banker
 from config.constant import strategy_provider as sp_constant
 from config.logger import get_logger
 from utility.utility import Utility
+from datetime import datetime
+import pandas as pd
 
 
 class Gambler(object):
     def __init__(self, gambler_id, principle, strategy_provider):
-        self.logger = get_logger('gambler_{}'.format(self.gambler_id))
+        self.logger = get_logger('gambler_{}'.format(gambler_id))
         self.gambler_id = gambler_id
         self.principle = principle
-        self.config = Utility.get_config()
+        self.config = Utility().get_config()
 
         self.strategy_provider = strategy_provider
-        self.battle_history = pd.DataFrame(columns=player_constant.battle_target)
+        self.decision_history = []
 
-    def n_battle(self, start_date):
-        gamble_info = Banker().get_gamble_info(game_date=start_date)
+    def battle(self, start_date, end_date=datetime.today().strftime('%Y%m%d')):
+        for game_date in pd.date_range(datetime.strptime(start_date, '%Y%m%d'), datetime.strptime(end_date, '%Y%m%d')):
+            game_date = game_date.strftime('%Y%m%d')
+            gamble_info = Banker().get_gamble_info(game_date=game_date)
 
-        # [decision, decision, ...]
-        decisions = self.strategy_provider.get_decision(self, gamble_info)
-        self.settle(decisions)
+            # [decision, decision, ...]
+            decisions = self.strategy_provider.get_decision(self, gamble_info)
+            self.decision_history += decisions
+            self.settle(decisions)
 
     def settle(self, decisions):
         self.logger.debug('start settle')
         for decision in decisions:
-            game_result = Banker().get_gamble_result(decision.gamble_id)
-            for banker_side in decision:
-                for bet_type, bet_decision in banker_side.items():
-                    self.logger.debug('settle game: %s' % decision)
-                    if bet_decision[0] == game_result[banker_side][bet_type]:
-                        decision.match = True
-                        gamble_info = Banker().get_gamble_info(game_date=decision.game_date,
-                                                               gamble_id=decision.gamble_id)
-                        response_ratio = gamble_info.handicap[banker_side][bet_type].get('response', 1.7)
-                        self.principle += bet_decision[1] * response_ratio
-                    else:
-                        decision.match = False
-                        self.principle -= bet_decision[1]
+            gamble_result = Banker().get_gamble_result(decision.game_date, decision.gamble_id)
+            for bet in decision.bets:
+                if bet.result == gamble_result.judgement[bet.banker_side][bet.type]:
+                    decision.match = True
+                    gamble_info = Banker().get_gamble_info(game_date=decision.game_date,
+                                                           gamble_id=decision.gamble_id)
+                    response_ratio = gamble_info.handicap[bet.banker_side][bet.type].get('response', 1.7)
+                    self.principle += bet.unit * response_ratio
+                else:
+                    decision.match = False
+                    self.principle -= bet.unit
+            self.logger.debug('settled decision: %s' % decision)
+            self.logger.debug('current principle: %s' % self.principle)
 
-    def battle(self, game_judgement):
-        self.logger.info('start battle'.format(self.gambler_id))
-        for row_id, game_result in game_judgement.iterrows():
-            kwargs = self.bet_strategy_kwargs(game_judgement, row_id)
-            bet = self.strategy_provider.get_bet_decision(self.bet_strategy, **kwargs)
-            battle_result = bet == game_result
-            self.battle_history.loc[row_id] = battle_result.astype(int)
-        self.logger.debug('finished battle, runs: {}'.format(len(self.battle_history)))
-        return self.battle_history
-
-    def bet_strategy_kwargs(self, game_judgement, row_id):
-        self.logger.debug('start generate ber strategy kwargs')
-        if self.bet_strategy.name == sp_constant.low_of_large:
-            return {'game_history': game_judgement[game_judgement.index < row_id]}
-        return dict()
+    # def battle(self, game_judgement):
+    #     self.logger.info('start battle'.format(self.gambler_id))
+    #     for row_id, game_result in game_judgement.iterrows():
+    #         kwargs = self.bet_strategy_kwargs(game_judgement, row_id)
+    #         bet = self.strategy_provider.get_bet_decision(self.bet_strategy, **kwargs)
+    #         battle_result = bet == game_result
+    #         self.battle_history.loc[row_id] = battle_result.astype(int)
+    #     self.logger.debug('finished battle, runs: {}'.format(len(self.battle_history)))
+    #     return self.battle_history
 
     def summarize_battle_history(self):
         self.logger.debug('start summarize battle history')
         full_strategy_name = self.bet_strategy.name if self.bet_strategy.name != sp_constant.low_of_large \
             else '{}_{}'.format(self.bet_strategy.name, self.bet_strategy.kwargs['recency'])
 
-        return pd.Series([full_strategy_name]).append(self.battle_history.mean().round(3)).values
+        return pd.Series([full_strategy_name]).append(self.decision_history.mean().round(3)).values
