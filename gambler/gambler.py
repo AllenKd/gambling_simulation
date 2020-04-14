e1f0073c89e2a09fb87f6e5936fb7052930b033d
allen <kaodondon7@gmail.com>
2020-04-15 02:40:29 +0800

    add dbindex

--- gambler/gambler.py
+++ gambler/gambler.py
@@ -1,11 +1,11 @@
+import json
 from datetime import datetime
 
 import pandas as pd
-from gambler.gamble_record import GambleRecord
 
 from banker.banker import Banker
 from config.logger import get_logger
-import json
+from gambler.gamble_record import GambleRecord
 from util.util import Util
 
 
@@ -66,7 +66,8 @@ class Gambler(object):
     def write_record(self, record):
         self.logger.debug(f"save decision to mongodb: {record}")
         # a = json.dumps(record.content, default=lambda o: o.__dict__)
-        self.mongo_client.insert_one(json.loads(json.dumps(record.content, default=lambda o: o.__dict__)))
+        doc = json.loads(json.dumps(record.content, default=lambda o: o.__dict__))
+        self.mongo_client.update(doc, doc, upsert=True)
 
     def summarize_battle_history(self):
         self.logger.debug("start summarize battle history")
