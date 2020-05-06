da6e46009a2f2242d26b2cc38726e4f99f9e1c82
allen <kaodondon7@gmail.com>
2020-05-06 20:55:41 +0800

    add kelly pur strategy

--- strategy_provider/put_strategy/kelly.py
+++ strategy_provider/put_strategy/kelly.py
@@ -1,3 +1,28 @@
-# response_ratio = self.ratio_per_game if not response_ratio else response_ratio
-#         bet_ratio = (win_prob * (response_ratio + 1) - 1) / response_ratio
-#         return round(bet_ratio * chips, -2)
+from strategy_provider.common.base_put_strategy import BasePutStrategy
+from util.util import Util
+
+
+class Kelly(BasePutStrategy):
+    """
+    Strategy Description:
+        Bet according to Kelly Formula
+
+    Suitable Bet Strategies:
+        One bet(decision) per day.
+    """
+
+    def __init__(self):
+        super().__init__("Kelly")
+
+    def get_unit(self, gamble_info, decision, gambler, base_strategy, **kwargs):
+        response = gamble_info.handicap[decision.bet.banker_side][decision.bet.type][
+            "response"
+        ][decision.bet.result]
+        # TODO: calculate win_prob by confidence and response ratio
+        win_prob = Util.confidence_to_prob(decision.confidence)
+        bet_ratio = (win_prob * (response + 1) - 1) / response
+
+        # do not bet if the ratio less than 0
+        bet_ratio = max(bet_ratio, 0)
+        self.logger.debug(f"bet ratio: {bet_ratio}")
+        return int(gambler.principle * bet_ratio)
