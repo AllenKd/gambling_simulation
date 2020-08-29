ebce637fc5be2b2f74c8a9c2edc412e30044e5aa
Allen <kaodondon7@gmail.com>
2020-08-30 00:36:04 +0800

    refine base strategy

--- strategy_provider/common/base_bet_strategy.py
+++ strategy_provider/common/base_bet_strategy.py
@@ -1,15 +1,15 @@
+import logging
 from abc import abstractmethod, ABC
-
-from config.logger import get_logger
 
 
 class BaseStrategy(ABC):
     def __init__(self, name, put_strategy):
-        self.logger = get_logger(self.__class__.__name__)
         self.name = name
         self.put_strategy = put_strategy
         self.parameters = None
-        self.logger.debug(f"strategy initialized: {self.name}, put strategy: {self.put_strategy.name}")
+        logging.debug(
+            f"strategy initialized: {self.name}, put strategy: {self.put_strategy.name}"
+        )
 
     @abstractmethod
     def get_decisions(self, gambler, gamble_info):
